5a0d192a6d1fa5af4a03790fe931229fe123b01b
jarvis4130 <atharvaadam101010@gmail.com>
2024-04-09 13:32:25 +0530

    2

--- backend/server.js
+++ backend/server.js
@@ -90,7 +90,7 @@ app.use("/api/bookings", bookingRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+  // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"))
